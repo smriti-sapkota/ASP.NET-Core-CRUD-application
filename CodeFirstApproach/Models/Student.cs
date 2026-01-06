6eec5c7ec5ece379cf7ad16aa79a99cf0a60214e
Smriti <sapkotasmriti56@gmail.com>
2026-01-06 20:31:48 +0545

    Add project files.

---
+++ CodeFirstApproach/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstApproach.Models
+{
+    public class Student
+    {
+        [Key]//makes primary key in database
+        public int Id { get; set; }
+
+        [Column("StudentName", TypeName = "varchar(100)")]
+
+        [Required]
+        public string Name { get; set; }
+
+        [Column("StudentGender", TypeName = "varchar(20)")]
+        [Required]
+
+        public string Gender { get; set; }
+
+        [Column("StudentAge", TypeName = "int(20)")]
+        [Required]
+        public  int? Age { get; set; }
+
+        [Required]
+        public  int? Standard{ get; set; }
+    }
+}
---
+++ CodeFirstApproach/Models/StudentDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApproach.Models
+{
+    public class StudentDBContext : DbContext //base is used to call the parent class constructor
+    {
+        public StudentDBContext(DbContextOptions options): base(options)//constructor = class name
+        {
+            
+        }
+
+        public  DbSet<Student> Students { get; set; } //Represents table data with name Students
+    }
+}
